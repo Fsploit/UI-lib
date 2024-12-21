7250c999895946e7e68fe2983974c297806302a1
Fsploit (retarded as fuck) <169143383+Fsploit@users.noreply.github.com>
2024-12-21 21:21:25 +0800

    Update Bleachhack.lua

--- Bleachhack.lua
+++ Bleachhack.lua
@@ -3,11 +3,13 @@ local runService = game:GetService("RunService")
 local userInputService = game:GetService("UserInputService")
 local debris = game:GetService("Debris")
 local statsService = game:GetService("Stats")
+local httpService = game:GetService("HttpService")
 
 local player = players.LocalPlayer
+local categoryDragging = false
 
 local Bleachhack = {}; Bleachhack.__index = Bleachhack
-local objects = game:GetObjects("rbxassetid://17799632030")[1]:Clone()
+local objects = (runService:IsStudio() and game:GetService("ReplicatedStorage"):FindFirstChild("Bleachhack") or game:GetObjects("rbxassetid://17799632030")[1]):Clone()
 
 local IS_STUDIO = runService:IsStudio()
 local indent = " "
@@ -76,7 +78,7 @@ end)
 
 local Category = {}; Category.__index = Category; do
 	local Module = {}; Module.__index = Module; do
-		function Module.new(title, root)
+		function Module.new(title, root, non, f)
 			local self = setmetatable({}, Module)
 			self.Title = title
 			self.Root = root
@@ -84,6 +86,7 @@ local Category = {}; Category.__index = Category; do
 			self.UI.Module.Text = indent..title
 			self.UI.Parent = root.UI.List
 			self.Value = false
+			self.Settings = {}
 			
 			local moduleTextLabel = objects.ModuleList:Clone()
 			moduleTextLabel.Visible = false
@@ -95,9 +98,29 @@ local Category = {}; Category.__index = Category; do
 			self.Root.UI.List.Size += UDim2.new(0, 0, 0, 23)
 			self.TextLabel = moduleTextLabel
 			
+			self.UI.Module.Dropdown.Visible = false
+			
+			self.Update = function(a)
+				self.Value = not self.Value
+				if not non then
+					self.UI.Module.TextColor3 = self.Value and Color3.fromHex("#6fecdd") or Color3.fromHex("#fff")
+				end
+				moduleTextLabel.Visible = self.Value
+				self.Root.Root.UI.ModulesList.Size += UDim2.new(0, 0, 0, 25 * (self.Value and 1 or -1))
+				if not a then
+					playSound(clickId, .5)
+				end
+				
+				if not a then
+					(f or function() end)(a)
+				end
+			end
+			
 			local dropdowned = false
 			
 			local function toggleDropdown()
+				if not self.UI.Module.Dropdown.Visible then return end
+				
 				dropdowned = not dropdowned
 				self.UI.Dropdown.Visible = dropdowned
 				self.UI.Size += UDim2.new(0, 0, 0, (#self.UI.Dropdown.Frame:GetChildren() - 1) * 23 * (dropdowned and 1 or -1))
@@ -108,11 +131,7 @@ local Category = {}; Category.__index = Category; do
 			end
 			
 			self.UI.Module.MouseButton1Click:Connect(function()
-				self.Value = not self.Value
-				self.UI.Module.TextColor3 = self.Value and Color3.fromHex("#6fecdd") or Color3.fromHex("#fff")
-				moduleTextLabel.Visible = self.Value
-				self.Root.Root.UI.ModulesList.Size += UDim2.new(0, 0, 0, 25 * (self.Value and 1 or -1))
-				playSound(clickId, .5)
+				self.Update()
 			end)
 			
 			self.UI.Module.Dropdown.MouseButton1Click:Connect(toggleDropdown)
@@ -130,6 +149,7 @@ local Category = {}; Category.__index = Category; do
 			ui.Parent = self.UI.Dropdown.Frame
 			
 			self.UI.Dropdown.Size += UDim2.new(0, 0, 0, 23)
+			self.UI.Module.Dropdown.Visible = true
 			
 			data.Update = function()
 				ui.TextColor3 = data.Value and Color3.fromHex("#54fc54") or Color3.fromHex("#fc5454")
@@ -144,6 +164,27 @@ local Category = {}; Category.__index = Category; do
 			
 			data.Update()
 			
+			self.Settings[data.Title] = data
+			
+			return data
+		end
+		
+		function Module:CreateButton(data)
+			local title = data.Title
+			local callback = data.Callback or function() end
+
+			local ui = objects.Toggle:Clone()
+			ui.Text = indent..title
+			ui.Parent = self.UI.Dropdown.Frame
+
+			self.UI.Dropdown.Size += UDim2.new(0, 0, 0, 23)
+			self.UI.Module.Dropdown.Visible = true
+
+			ui.MouseButton1Click:Connect(function()
+				playSound(clickId, .5)
+				callback(data.Value)
+			end)
+
 			return data
 		end
 		
@@ -158,6 +199,7 @@ local Category = {}; Category.__index = Category; do
 			ui.Parent = self.UI.Dropdown.Frame
 			
 			self.UI.Dropdown.Size += UDim2.new(0, 0, 0, 23)
+			self.UI.Module.Dropdown.Visible = true
 			
 			data.Update = function()
 				ui.Text = indent..title..": "..data.Value.Name
@@ -172,6 +214,8 @@ local Category = {}; Category.__index = Category; do
 			end)
 			
 			data.Update()
+			
+			self.Settings[data.Title] = data
 			
 			return data
 		end
@@ -189,6 +233,7 @@ local Category = {}; Category.__index = Category; do
 			ui.Parent = self.UI.Dropdown.Frame
 
 			self.UI.Dropdown.Size += UDim2.new(0, 0, 0, 23)
+			self.UI.Module.Dropdown.Visible = true
 
 			data.Update = function()
 				ui.Text = indent..title..": "..data.Value
@@ -200,7 +245,6 @@ local Category = {}; Category.__index = Category; do
 				if rangeIndex > #range then
 					rangeIndex = 1
 				end
-				print(range, rangeIndex)
 				data.Value = range[rangeIndex]
 				data.Update()
 				playSound(clickId, .5)
@@ -208,6 +252,8 @@ local Category = {}; Category.__index = Category; do
 			end)
 
 			data.Update()
+			
+			self.Settings[data.Title] = data
 
 			return data
 		end
@@ -230,6 +276,7 @@ local Category = {}; Category.__index = Category; do
 			ui.Parent = self.UI.Dropdown.Frame
 
 			self.UI.Dropdown.Size += UDim2.new(0, 0, 0, 23)
+			self.UI.Module.Dropdown.Visible = true
 
 			data.Update = function()
 				local percentage = (data.Value - range[1]) / (range[2] - range[1])
@@ -257,8 +304,17 @@ local Category = {}; Category.__index = Category; do
 			end)
 			
 			ui.Text.InputBegan:Connect(function(input)
-				if input.UserInputType == Enum.UserInputType.MouseButton1 then
+				if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
 					dragging = true
+					local mousePos = userInputService:GetMouseLocation()
+					local mouseX, mouseY = mousePos.X, mousePos.Y
+					local boundaries0 = ui.Text.AbsolutePosition.X 
+					local boundaries1 = ui.Text.AbsolutePosition.X + ui.Text.AbsoluteSize.X
+					local at = mouseX - boundaries0
+					local goal = boundaries1 - boundaries0
+					local percentage = math.clamp(at / goal, 0, 1)
+					data.Value = (nonDecimal and math.round or function(v) return v end)(rangeMin + ((rangeMax - rangeMin) * percentage))
+					data.Update()	
 					local e; e = input.Changed:Connect(function()
 						if input.UserInputState == Enum.UserInputState.End then
 							dragging = false
@@ -267,6 +323,8 @@ local Category = {}; Category.__index = Category; do
 					end)
 				end
 			end)
+			
+			self.Settings[data.Title] = data
 
 			return data
 		end
@@ -282,6 +340,14 @@ local Category = {}; Category.__index = Category; do
 		self.UI.Position += UDim2.new(0, 180 * #root.UI.Modules:GetChildren(), 0, 0)
 		self.UI.Parent = root.UI.Modules
 		self.Root = root
+		self.Modules = {}
+		
+		if root.UI.Modules.AbsoluteSize.X < 180 * (#root.UI.Modules:GetChildren() - 1) and not string.match(getexecutorname():lower(), "macsploit") then
+			if not getfenv().A then
+				getfenv().A = #root.UI.Modules:GetChildren() - 1
+			end
+			self.UI.Position = UDim2.new(0, (#root.UI.Modules:GetChildren() - 1 - (getfenv().A)) * 180, 0, 200)
+		end
 		
 		do
 			local gui = self.UI
@@ -297,6 +363,8 @@ local Category = {}; Category.__index = Category; do
 			end
 
 			gui.Title.InputBegan:Connect(function(input)
+				if categoryDragging then return end
+				categoryDragging = true
 				if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
 					dragging = true
 					dragStart = input.Position
@@ -304,6 +372,7 @@ local Category = {}; Category.__index = Category; do
 
 					input.Changed:Connect(function()
 						if input.UserInputState == Enum.UserInputState.End then
+							categoryDragging = false
 							dragging = false
 						end
 					end)
@@ -326,8 +395,10 @@ local Category = {}; Category.__index = Category; do
 		return self
 	end
 	
-	function Category:CreateModule(title)
-		return Module.new(title, self)
+	function Category:CreateModule(title, n, f)
+		local module = Module.new(title, self, n, f)
+		self.Modules[title] = module
+		return module
 	end
 end
 
@@ -335,6 +406,7 @@ function Bleachhack:Create()
 	local self = setmetatable({}, Bleachhack)
 	self.UI = objects.Bleachhack:Clone()
 	self.UI.Parent = IS_STUDIO and player.PlayerGui or ((gethui and gethui()) or game:GetService("CoreGui"))
+	self.Categories = {}
 	
 	local function toggleModules()
 		self.UI.Modules.Visible = not self.UI.Modules.Visible
@@ -369,7 +441,54 @@ function Bleachhack:Create()
 end
 
 function Bleachhack:CreateCategory(title, icon)
-	return Category.new(title, icon, self)
+	local category = Category.new(title, icon, self)
+	self.Categories[title] = category
+	return category
+end
+
+function Bleachhack:Export()
+	local data = {}
+	
+	for index, category in pairs(self.Categories) do
+		data[category.Title] = {}
+		for index, module in pairs(category.Modules) do
+			data[category.Title][module.Title] = {
+				Value = module.Value,
+				Settings = {}
+			}
+			for index, setting in pairs(module.Settings) do
+				local serialized = setting.Value
+				
+				if typeof(serialized) == "EnumItem" then
+					serialized = {
+						Type = "EnumItem",
+						Name = serialized.Name
+					}
+				end
+				
+				data[category.Title][module.Title].Settings[index] = serialized
+			end
+		end
+	end
+	
+	return httpService:JSONEncode(data)
+end
+
+function Bleachhack:Import(data)
+	data = httpService:JSONDecode(data)
+	
+	for categoryName, data in pairs(data) do
+		if not self.Categories[categoryName] then continue end
+		for moduleName, moduleData in pairs(data) do
+			if not self.Categories[categoryName].Modules[moduleName] then continue end
+			self.Categories[categoryName].Modules[moduleName].Value = not moduleData.Value
+			self.Categories[categoryName].Modules[moduleName].Update(true)
+			for index, value in pairs(moduleData.Settings) do
+				self.Categories[categoryName].Modules[moduleName].Settings[index].Value = typeof(value) == "table" and value.Type == "EnumItem" and Enum.KeyCode[value.Name] or value
+				self.Categories[categoryName].Modules[moduleName].Settings[index].Update()
+			end
+		end
+	end
 end
 
 return Bleachhack
